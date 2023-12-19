aea977ea7370e5f9df3348c8b7f8d0721b7feffc
Fuad Hasan <fuad.cs22@gmail.com>
2023-12-19 18:41:15 +0600

    ⚒️ Service worker

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,24 @@
 <script lang="ts">
     import "$lib/global.css";
+    import { onMount } from "svelte";
+
+    async function detectSWUpdate(){
+        const registration = await navigator.serviceWorker.ready;
+
+        registration.addEventListener("updatefound", () => {
+            const newWorker = registration.installing;
+            newWorker?.addEventListener("statechange", () => {
+                if (newWorker.state === "installed") {
+                    newWorker.postMessage({ type: "SKIP_WAITING" });
+                    console.log("New update available");
+                }
+            });
+        });
+    }
+
+    onMount(() => {
+        detectSWUpdate();
+    })
 </script>
 
 <slot></slot>
