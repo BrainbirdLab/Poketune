5e16f8a4d9658ad77df2813c0963845d28e98b4f
Fuad Hasan <fuad.cs22@gmail.com>
2023-12-23 19:18:42 +0600

    .

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,7 +7,7 @@
 
 <svelte:head>
     <title>PokeTune</title>
-    <!-- Update icon. -->
+    <!-- Update icon -->
     <link rel="icon" type="image/png" href="/images/icon(custom).png" />
 </svelte:head>
 
