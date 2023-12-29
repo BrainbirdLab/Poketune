dbe845fd9e11b17f33b9d960b898e0a373c023d3
Fuad Hasan <fuad.cs22@gmail.com>
2023-12-29 15:45:29 +0600

    attr

--- src/lib/components/SelectInstrument.svelte
+++ src/lib/components/SelectInstrument.svelte
@@ -25,14 +25,38 @@
         {/each}
     </div>
 </div>
-<div class="footer" in:fade>
-    <a href="https://github.com/itsfuad/Poketune" target="_blank" rel="noopener noreferrer">
-        <span class="v">Build v{version}</span>
-        <i class="fa-solid fa-code"></i>
-    </a>
-</div>
+
+<footer class="footer">
+    <div class="source"><a href="https://github.com/itsfuad/Poketune">Source code <i class="fa-solid fa-code"></i></a></div>
+    ・
+    <a href="https://www.flaticon.com/free-icons/course" title="course icons">Icon by Freepik - Flaticon</a>
+</footer>
 
 <style lang="scss">
+
+    .footer{
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: center;
+		width: 100%;
+		padding: 10px;
+		gap: 5px;
+		font-size: 0.6rem;
+        position: relative;
+		*{
+			padding: 0;
+			color: ghostwhite;
+		}
+
+		a{
+			text-decoration: underline;
+		}
+		i{
+			font-size: 0.7rem;
+		}
+	}
+
     .title {
         font-size: 1.3rem;
         color: #fff;
@@ -57,24 +81,6 @@
             display: flex;
             align-items: center;
             gap: 10px;
-        }
-    }
-    .footer {
-        position: absolute;
-        bottom: 0;
-        left: 0;
-        right: 0;
-        padding: 10px;
-        text-align: center;
-        font-size: 0.6rem;
-        color: #fff;
-        background: var(--primary);
-        .v{
-            position: relative;
-        }
-        a {
-            color: #fff;
-            text-decoration: underline;
         }
     }
 
