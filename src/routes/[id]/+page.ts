1a4f9ed9600d89f1dc0003ce61098fdca0848693
Fuad Hasan <fuad.cs22@gmail.com>
2023-12-31 22:43:32 +0600

    ..

--- src/routes/[id]/+page.ts
+++ src/routes/[id]/+page.ts
@@ -2,11 +2,8 @@ import { instrumentNames, sentenceCase } from '$lib/store';
 import { redirect } from '@sveltejs/kit';
 
 export function load({params}){
-
-    console.log('params.id: ' + params.id);
-
+    
     const instrumentName = sentenceCase(params.id as string);
-    console.log('instrumentName: ' + instrumentName);
     //if params.id is in InstrumentTypes type
     if (instrumentNames.includes(instrumentName)) {
         return {
